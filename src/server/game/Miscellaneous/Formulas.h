8d47ee4e91e34e0927789acbccd8d3e44bdfde07
Andrei Streltsov <andrei@astreltsov.com>
2017-04-16 15:31:10 +0300

    adjusted XP to 1-man

--- src/server/game/Miscellaneous/Formulas.h
+++ src/server/game/Miscellaneous/Formulas.h
@@ -163,6 +163,14 @@ namespace Trinity
             Creature* creature = u->ToCreature();
             uint32 gain = 0;
 
+            // XP adjustment
+            float xpAdjust = 1.0f;
+            uint32 mapId = creature->GetMap()->GetId();
+            if(_instances5Man.find(mapId) != _instances5Man.end()){
+                xpAdjust = sWorld->getRate(RATE_XP_KILL_5MANOLD);
+            }
+
+
             if (!creature || (!creature->IsTotem() && !creature->IsPet() && !creature->IsCritter() &&
                 !(creature->GetCreatureTemplate()->flags_extra & CREATURE_FLAG_EXTRA_NO_XP_AT_KILL)))
             {
@@ -184,7 +192,7 @@ namespace Trinity
                     xpMod *= creature->GetCreatureTemplate()->ModExperience;
                 }
 
-                xpMod *= isBattleGround ? sWorld->getRate(RATE_XP_BG_KILL) : sWorld->getRate(RATE_XP_KILL);
+                xpMod *= isBattleGround ? sWorld->getRate(RATE_XP_BG_KILL) : sWorld->getRate(RATE_XP_KILL)*xpAdjust;
                 if (creature && creature->m_PlayerDamageReq) // if players dealt less than 50% of the damage and were credited anyway (due to CREATURE_FLAG_EXTRA_NO_PLAYER_DAMAGE_REQ), scale XP gained appropriately (linear scaling)
                     xpMod *= 1.0f - 2.0f*creature->m_PlayerDamageReq / creature->GetMaxHealth();
 
